56bf16d79e73199f660cd0bc4480e1b3b938967b
yarentamir <yarden44441@gmail.com>
2021-11-06 15:33:12 +0200

    scroll effect

--- js/main.js
+++ js/main.js
@@ -1,24 +1,76 @@
-const items = document.querySelectorAll("#timeline li");
+// ! show when scroll down
+function Utils() {}
+Utils.prototype = {
+  constructor: Utils,
+  isElementInView: function (element, fullyInView) {
+    var pageTop = $(window).scrollTop();
+    var pageBottom = pageTop + $(window).height();
+    var elementTop = $(element).offset().top;
+    var elementBottom = elementTop + $(element).height();
 
-const isInViewport = (el) => {
-  const rect = el.getBoundingClientRect();
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <=
-      (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
+    if (fullyInView === true) {
+      return pageTop < elementTop && pageBottom > elementBottom;
+    } else {
+      return elementTop <= pageBottom && elementBottom >= pageTop;
+    }
+  },
 };
 
-const run = () =>
-  items.forEach((item) => {
-    if (isInViewport(item)) {
-      item.classList.add("show");
+var Utils = new Utils();
+$(window).on("load", addFadeIn());
+
+$(window).scroll(function () {
+  addFadeIn(true);
+});
+
+function addFadeIn(repeat) {
+  var classToFadeIn = ".project-card";
+  $(classToFadeIn).each(function (index) {
+    var isElementInView = Utils.isElementInView($(this), false);
+    if (isElementInView) {
+      if (!$(this).hasClass("fadeInRight") && !$(this).hasClass("fadeInLeft")) {
+        if (index % 2 == 0) $(this).addClass("fadeInRight");
+        else $(this).addClass("fadeInLeft");
+      }
+    } else if (repeat) {
+      $(this).removeClass("fadeInRight");
+      $(this).removeClass("fadeInLeft");
     }
   });
+}
 
-// Events
-window.addEventListener("load", run);
-window.addEventListener("resize", run);
-window.addEventListener("scroll", run);
+// ! animated navbar
+const navbar = document.getElementById("navbar");
+let scrolled = false;
+
+window.onscroll = function () {
+  if (window.pageYOffset > 80) {
+    navbar.classList.remove("top");
+    navbar.classList.add("white-txt");
+    if (!scrolled) {
+      navbar.style.transform = "translateY(-70px)";
+    }
+    setTimeout(function () {
+      navbar.style.transform = "translateY(0)";
+      scrolled = true;
+    }, 200);
+  } else {
+    navbar.classList.add("top");
+    navbar.classList.remove("white-txt");
+    scrolled = false;
+  }
+};
+
+// ! Smooth Scrolling
+$("#con-btn").on("click", function (event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+    const contDiv = $(".section3-container");
+    $("html, body").animate(
+      {
+        scrollTop: $(contDiv).offset().top,
+      },
+      800
+    );
+  }
+});
